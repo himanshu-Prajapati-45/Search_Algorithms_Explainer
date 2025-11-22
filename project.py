977dbdc4c4eedb0ab45aed96b23e2654bd250344
himanshuhimanshu-Prajapati-45 <himanshu9649yash@gmail.com>
2025-11-22 21:16:47 +0530

    JSON loading and improve error handling

--- project.py
+++ project.py
@@ -12,3 +12,29 @@
 
 #-------------------------------------------------
 
+import json
+import os
+
+def load_questions(filename):     
+    
+    if not os.path.exists(filename):
+        print(f"Warning: {filename} not found.")
+        return {}
+    
+    try:
+        with open(filename,"r",encoding="utf-8") as file:
+            data = json.load(file)
+    
+    except Exception:
+        print(f"Error: Unable to read'{filename}'.")    
+        return {}
+    
+    if not isinstance(data,dict):
+        print("Error: JSON must contain key-value pair.")
+        return {}
+
+    return data
+
+if __name__ == "__main__":
+    questions = load_questions("questions.json")
+    print("KEYS:",list(questions.keys()))
