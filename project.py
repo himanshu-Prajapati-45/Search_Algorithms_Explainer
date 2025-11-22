06d0384abd31d72bf86a155a8dda74f93d1fb34f
himanshuhimanshu-Prajapati-45 <himanshu9649yash@gmail.com>
2025-11-22 19:06:11 +0530

    Project Header And Features

---
+++ project.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------
+# Search Algorithms Explainer - Python (CLI) 
+#-------------------------------------------------
+
+# Features ----->
+
+# - Linear Search 
+# - Binary Search
+# - Comparing
+# - History
+#- Argparse CLI Commands
+
+#-------------------------------------------------
+
