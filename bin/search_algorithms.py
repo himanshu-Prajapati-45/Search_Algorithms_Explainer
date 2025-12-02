eafd2d5b2dfd5c990fb93a1296a5db7f4576073f
himanshuhimanshu-Prajapati-45 <himanshu9649yash@gmail.com>
2025-12-03 00:49:15 +0530

    Add essential files for CLI functionality

---
+++ bin/search_algorithms.py
@@ -0,0 +1,8 @@
+#------------------------------------#
+#Launcher file for search algorithms
+#------------------------------------#
+
+from search_algorithms.__main__ import main
+
+if __name__ == "__main__":
+    main()
